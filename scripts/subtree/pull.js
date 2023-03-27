1fd8d14e4b4fb16ce2d01f1490830d752d02d27b
mmicome <mmicome@gmail.com>
2023-03-27 23:19:19 +0800

    initial

--- scripts/subtree/pull.js
+++ scripts/subtree/pull.js
@@ -1,6 +1,6 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 const { cwd } = require('node:process');
 
 fs.readdir(cwd(), (error, files) => {
@@ -8,22 +8,22 @@ fs.readdir(cwd(), (error, files) => {
     console.log(`error: ${error.message}`);
     return;
   }
-  files.forEach(file => {
-    if(/codehub-*/.test(file)) {
-      exec(`git subtree pull --prefix=${file} ${file} main --squash`, (error, stdout, stderr) => {
-          if (error) {
-              console.log(`error: ${error.message}`);
-              return;
-          }
-          if (stderr) {
-              console.log(`stderr: ${stderr}`);
-              return;
-          }
-          console.log(`stdout: ${stdout}`);
+  files.forEach((file) => {
+    if (/codehub-*/.test(file)) {
+      exec(`git subtree pull --prefix=${file} ${file} main`, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
       });
     }
-  })
-})
+  });
+});
 
 // exec("git subtree push --prefix=codehub-commons codehub-commons main", (error, stdout, stderr) => {
 //     if (error) {
