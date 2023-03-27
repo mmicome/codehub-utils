6241fbd7f45a03615900230c63c4491b61c0ef18
mmicome <mmicome@gmail.com>
2023-03-27 22:32:29 +0800

    Squashed 'codehub-monitor/' changes from 09a40a9..d39b3fb
    d39b3fb init
    53ea592 init
    9a07f0b Initial commit
    REVERT: 09a40a9 initial
    REVERT: 856b55e init
    REVERT: 9dbd05f init
    REVERT: 27cd2c7 Initial commit
    
    git-subtree-dir: codehub-monitor
    git-subtree-split: d39b3fb33a0fb6cbbfb7ba364c420714529181ca

--- src/config/data-source-mongodb.ts
+++ src/config/data-source-mongodb.ts
@@ -1,15 +1,11 @@
-import { DataSource } from 'typeorm';
-import path from 'path';
-const entities = path.resolve(__dirname, '../model/mongodb/*.entity.{ts,js}');
+import { DataSource } from "typeorm";
+import path from 'path'
+const entities = path.resolve(__dirname, "../model/mongodb/*.entity.{ts,js}");
 
 export const db = new DataSource({
-  type: 'mongodb',
-  host: '127.0.0.1',
-  port: 27017,
-  username: '',
-  password: '',
-  useUnifiedTopology: true,
-  database: 'test',
-  logging: ['error'], //"query",
+  type: "mongodb",
+  host: "localhost",
+  database: "test",
+  logging: ["query", "error"],
   entities: [entities],
-});
+})
