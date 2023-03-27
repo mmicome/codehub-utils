276c5ae265032b5e36c5b7078869d69cc7d7eaa3
mmicome <mmicome@gmail.com>
2023-03-27 23:47:02 +0800

    initial

--- src/database/mongodb.ts
+++
@@ -1,9 +0,0 @@
-import {mongodb} from '@config/index'
-
-mongodb.initialize()
-    .then(() => {
-        // here you can start to work with your database
-    })
-    .catch((error) => console.log(error))
-
-export default mongodb;
