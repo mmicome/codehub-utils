5c605b705b116becac291d95d48c40da7cdcdf98
mmicome <mmicome@gmail.com>
2023-04-06 20:06:24 +0800

    feat(gateway): gateway and sms do

--- src/utils/auto-load.ts
+++ src/utils/auto-load.ts
@@ -1,16 +1,22 @@
 // const glob = require('glob');
 // const path = require('path');
-import glob from 'glob'
+import glob from 'glob';
 
-export function load(dir:string) {
+export function load(dir: string, options: { exclude: string } = { exclude: 'dist/' }) {
   const files = glob.sync(dir, {
     // absolute: true,
-    withFileTypes: true
-  })
-  // Iterate over the matched files and require each one
-  files.forEach((file) => {
-    const component = import(file.fullpath());
-    // Extract the component name from the file name (e.g. "acl.component.js" => "acl")
-    // const componentName = path.basename(file, '.component.js');
+    withFileTypes: true,
   });
+
+  const libs = [];
+  // Iterate over the matched files and require each one
+  files
+    .filter((file) => !new RegExp(options.exclude).test(file))
+    .forEach((file) => {
+      const lib = require(file);
+      libs.push(lib);
+      // Extract the component name from the file name (e.g. "acl.component.js" => "acl")
+      // const componentName = path.basename(file, '.component.js');
+    });
+  return libs;
 }
